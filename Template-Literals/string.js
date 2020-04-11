4e5527ee5ffc889af1f60f1090a4012336ffd977
Arham <arham426@gmail.com>
2020-04-11 07:19:13 +0500

    first assignment

---
+++ Template-Literals/string.js
@@ -0,0 +1,11 @@
+// declare a variable without value 
+  var string1;
+
+// now assign value to string1 with multi-line dummy text
+ string1 = `Lorem ipsum dolor sit amet, 
+ consectetur adipisicing elit. Numquam obcaecati doloremque provident 
+ praesentium ipsum odio dignissimos corrupti, omnis quidem mollitia.`;
+
+//  now show it's value in console
+
+console.log(string1);
---
+++ Template-Literals/template.js
@@ -0,0 +1,18 @@
+// declare variables
+
+var MyName = "Syed Muhammad Arham Ali";
+var dob = "24_August_2001";
+var course = "Cloud Native Computing";
+var College = "Aisha Bawany Govt Science College";
+var org = "PIAIC";
+var goal = "Cloud Native Expert";
+
+var data = ` Hi! I am ${MyName}
+I was born on ${dob}
+I am Graduated from ${College}
+I am studing ${course} from ${org}
+I want to become a ${goal}
+`;
+// print value with sum-up 
+ 
+console.log(data);
